6a12d3e3580d231b3ed077350081737e7940242a
dmytromayevsky <mayevskydv@gmail.com>
2024-11-01 01:26:09 +0100

    part 2 done

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -9,8 +9,10 @@ export enum UserRole {
   guest = 'guest',
 }
 
+
+
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
+const RoleDescription:Record<UserRole, String> = {
   admin: 'Admin User',
   editor: 'Editor User',
   guest: 'Guest User',
